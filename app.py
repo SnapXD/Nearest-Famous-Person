ea7fd035b35b6e74d17bd4c7120a28270f120a07
sluchess <sluchess@eecs.wsu.edu>
2019-02-23 15:58:50 -0800

    Committing to switch branches.

--- app.py
+++ app.py
@@ -2,18 +2,19 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 import flask_sqlalchemy as sqlalchemy
 from flask import redirect, url_for
-from flask import Response
-from flask_login import LoginManager, UserMixin, current_user, login_user, login_required
-from getpass import getpass
-from flask import current_app
-import datetime
+# from flask import Response
+# from flask_login import LoginManager, UserMixin, current_user, login_user, login_required
+# from getpass import getpass
+# from flask import current_app
+# import datetime
 
 
 app = Flask(__name__)
-login_manager = LoginManager()
-login_manager.init_app(app)
+# login_manager = LoginManager()
+# login_manager.init_app(app)
 CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlalchemy-demo.db'
 
 db = sqlalchemy.SQLAlchemy(app)
 
+base_url = 'https://api.twitter.com/1.1/search/tweets.json'
